bf2ee3f608c3a32bde81f2b9cfbbd2dc60feaa26
Anuraag Manvi <anuraag.m.98@gmail.com>
2021-04-04 14:03:17 +0530

    Update summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -10,50 +10,62 @@ import urllib.request
 import re
 import nltk
 import heapq
-topic = input("What would you like to know today?")
-url = "https://en.wikipedia.org/wiki/" + topic
-source = urllib.request.urlopen(url).read()
 
-soup = bs.BeautifulSoup(source, 'lxml')
+while(True):
+    topic = input("What would you like to know today?")
+    if topic.lower() == 'quit':
+        exit(0)
+    topic = topic.title()
+    topic = re.sub(' ', '_', topic)
+    url = "https://en.wikipedia.org/wiki/" + topic
+    try:
+        source = urllib.request.urlopen(url).read()
+    except:
+        print("Looks like Wikipedia has not yet covered " + topic)
+        continue
 
-text = ""
-for para in soup.find_all('p'):
-    text += para.text
-    
-text = re.sub(r"\[[0-9]*\]", "", text)
-text = re.sub(r"\s+", " ", text)
+    soup = bs.BeautifulSoup(source, 'lxml')
 
-clean_text = text.lower()
-clean_text = re.sub(r"\W", " ", clean_text)
-clean_text = re.sub(r"\d", "", clean_text)
+    text = ""
+    for para in soup.find_all('p'):
+        text += para.text
 
-sent = nltk.sent_tokenize(text)
+    text = re.sub(r"\[[0-9]*\]", "", text)
+    text = re.sub(r"\s+", " ", text)
 
-stop_words = nltk.corpus.stopwords.words('english')
+    clean_text = text.lower()
+    clean_text = re.sub(r"\W", " ", clean_text)
+    clean_text = re.sub(r"\d", "", clean_text)
 
-word_to_count = {}
-for word in nltk.word_tokenize(clean_text):
-    if word not in stop_words:
-            if word not in word_to_count.keys():
-                word_to_count[word] = 1
-            else:
-                word_to_count[word] += 1
-                
-for key in word_to_count.keys():
-    word_to_count[key] = word_to_count[key]/max(word_to_count.values())
-    
-sent_to_score = {}
-for sentence in sent:
-    for word in nltk.word_tokenize(sentence.lower()):
-        if word in word_to_count.keys():
-            if len(sentence.split(' ')) < 25:
-                if sentence not in sent_to_score.keys():
-                    sent_to_score[sentence] = word_to_count[word]
+    sent = nltk.sent_tokenize(text)
+
+    stop_words = nltk.corpus.stopwords.words('english')
+
+    word_to_count = {}
+    for word in nltk.word_tokenize(clean_text):
+        if word not in stop_words:
+                if word not in word_to_count.keys():
+                    word_to_count[word] = 1
                 else:
-                    sent_to_score[sentence] += word_to_count[word]
-                    
-best_sent = heapq.nlargest(6, sent_to_score, key=sent_to_score.get)
+                    word_to_count[word] += 1
 
-print('---------------------------')
-for sentence in best_sent:
-    print(sentence)
+    for key in word_to_count.keys():
+        word_to_count[key] = word_to_count[key]/max(word_to_count.values())
+
+    sent_to_score = {}
+    for sentence in sent:
+        for word in nltk.word_tokenize(sentence.lower()):
+            if word in word_to_count.keys():
+                if len(sentence.split(' ')) < 25:
+                    if sentence not in sent_to_score.keys():
+                        sent_to_score[sentence] = word_to_count[word]
+                    else:
+                        sent_to_score[sentence] += word_to_count[word]
+
+    best_sent = heapq.nlargest(7, sent_to_score, key=sent_to_score.get)
+
+    print('---------------------------')
+    for sentence in best_sent:
+        print(sentence)
+    print('---------------------------')
+    print()
